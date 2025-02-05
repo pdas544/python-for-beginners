f7a46efe2d801c9f60cdbd7807d2370899dc3cda
priyabrata das <pdas544@gmail.com>
2025-02-05 09:58:38 +0530

    add intro and variables file

---
+++ intro.py
@@ -0,0 +1,39 @@
+'''
+Brief Introduction:
+- Python is a highl level programming language
+- Created by Guido von rossum
+
+Features:
+- Interpreted language
+- Dynamic typing
+- Object-oriented
+- Supports modules, packages, and exceptions
+
+Python used for:
+1. Web development
+2. Scientific computing
+3. Artificial Intelligence
+4. Machine learning
+5. Data analysis
+
+For eg: Youtube, Uber, Dropbox, and Pinterest and many more.
+'''
+
+#Basic Output example
+
+print("Hello, World!")  #"Hello, World!" is sent to the output screen
+
+#Comments in Python
+# It is a good programming practice to use comments
+# Comments are written in two ways: 
+# 1. Single-line comments using # symbol
+# 2. Multi-line comments using triple quote symbols ''' comment '''
+
+# for input example
+'''
+input function is used to take the input from the user and
+and store it in a variable which can be used later.
+'''
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
---
+++ variables-data-types.py
@@ -0,0 +1,33 @@
+'''
+Variables: 
+- Variables in Python are used to store data values. 
+- A variable is created the moment you first assign a value to it. 
+- Python is a dynamically typed language, which means you don't need to declare the type of a variable when you create one. 
+- The type is inferred from the value you assign to the variable. For example:
+
+x = 5       # x is of type integer
+y = "Hello" # y is of type text (aka string)
+
+Variables can change type after they have been set. For example:
+
+x = 4       # x is initially an integer
+x = "Four"  # x is now a string
+
+Python variable names are case-sensitive and can contain letters, numbers, and underscores, but they must start with a letter or an underscore.
+
+For eg: 
+lx = 5 # correct
+_x = 10 #correct
+3x = 15 # incorrect variable name 3x, must start with a letter
+'''
+
+number=5
+name = "Rahul"
+price = 3.2
+go_to_movie = True
+#3incorrect_variable_name : uncomment this line and run the program again
+#it will show you an error message
+print(number)
+print(name)
+print(price)
+print(go_to_movie)
