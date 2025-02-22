fea42b2986c04b536056cd9fc42b614d10e39e63
priyabrata das <pdas544@gmail.com>
2025-02-22 06:44:40 +0530

    add practice questions on conditional statements

---
+++ 06practice-conditionals.py
@@ -0,0 +1,20 @@
+'''
+Practice questions on conditionals
+
+Q1. Write a program to accept age from the user and check voting age. If the age is above 18, display "You can vote" else display "You cannot Vote"
+
+Q2. Write a program to check if a number is even or odd.
+
+Q3. Write a program to accept a four digit number and check whether it is leap year or not.
+
+Q4. Write a program to percentage and check the following:
+    - If the percentage is above 75, display "Excellent"
+    - If the percentage is between 75 and 65 (inclusive), display "Good"
+    - If the percentage is between 65 and 55 (inclusive), display "Satisfactory"
+    - If the percentage is below 55, display "Fail"
+
+Q5. Write a progam to accept three numbers and check the largest among them.
+
+
+
+'''
