48c444d1f6c2be7b35524e30a40312d40eca14ff
priyabrata das <pdas544@gmail.com>
2025-02-27 11:21:45 +0530

    modify readme and add practice questions on looping

---
+++ 07practice-looping.py
@@ -0,0 +1,37 @@
+'''
+Q1. Write a program to accept a string from the user and display it 5 times.
+
+Q2. Write a program to accept a number and check whether it is prime or not.
+
+Q3. Write a program to display the following
+
+a) 1,2,3, 4, 5 ... 20
+b) 20,19,18,17,...1
+c) 1,3,5,7,...19
+d) 1,4,9,16,...100
+
+Q4. Write a program to accept a number and display its multiplication table till 10
+
+Q5. Write a program to display the sum of first 10 numbers
+
+Q6. Write a program to display the factorial of a given number
+
+Q7. Write a program to display the first 10 elements of Fibonacci series.
+
+Q8. Write a program to accept a string from the user and count the number of vowels in it.
+
+Q9. Write a program to display prime numbers between "m" and "n". Both "m" and "n" will be provided by user.
+
+Q10. Write a program to display the following pattern:
+
+a)  1
+    1 2
+    1 2 3
+    1 2 3 4
+
+b)  1 2 3 4
+    1 2 3
+    1 2
+    1
+
+'''
--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Python-For-Beginners 🐍
----
-## Topics Covered
-1) Brief Introduction, Basic Input-Output 
-2) Variables and DataTypes
-3) Operators
-4) Conditional Statements
-5) Looping Statements
-6) Functions
-7) Data Strucutres - List, Tuple, Dictionary, Set
-8) Exception
-9) File Handling
-10) Object Oriented Programming
 
+---
+
+## Topics Covered
+
+1. Brief Introduction, Basic Input-Output
+2. Variables and DataTypes
+3. Operators
+4. Conditional Statements
+5. Looping Statements
+6. Practice Conditional Statements
+7. Practice Looping Statements
+8. Functions
+9. Data Strucutres - List, Tuple, Dictionary, Set
+10. Exception
+11. File Handling
+12. Object Oriented Programming
+13. Algorithms - Introduction
+14. Time and Space Complexity
