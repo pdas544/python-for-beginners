4764ebfb8ecce541e959ae7fd841d112b5a11388
priyabrata das <pdas544@gmail.com>
2025-03-30 21:43:08 +0530

    add new file 11functions-pratice.py

---
+++ 11functions-practice.py
@@ -0,0 +1,12 @@
+'''
+Questions for practicing functions
+1. Write a function that takes a list of numbers and returns the sum of the numbers.
+2. Write a function that takes a string and returns the string reversed without using the built-in reverse function.
+4. Write a function that takes a list of numbers and returns the average of the numbers.
+5. Write a function that takes a string and returns the number of vowels in the string.
+6. Write a function that takes a list of numbers and returns the maximum number.
+7. Write a function that takes a list of numbers and returns the minimum number.
+8. Write a function that takes a string and returns the number of consonants in the string.
+9. Write a function that takes a list of numbers and returns the median of the numbers.
+10. Write a function that takes a string and returns the number of words in the string.
+'''
