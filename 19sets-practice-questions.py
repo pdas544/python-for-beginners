c1eb96fe6c440a51a7f477c8009b669effdc6716
priyabrata das <pdas544@gmail.com>
2025-05-01 11:00:38 +0530

    add practice questions on sets and dictionaries

---
+++ 19sets-practice-questions.py
@@ -0,0 +1,11 @@
+'''
+practice questions for sets
+1. Create a set of numbers from 1 to 10.
+4. Create a set of prime numbers from 1 to 50.
+5. Create a set of squares of numbers from 1 to 10.
+
+advance questions on sets
+
+1. Create a set of numbers from 1 to 10 and find the sum of all the numbers in the set.
+2. Create a set of numbers from 1 to 10 and find the maximum and minimum number in the set.
+'''
---
+++ 20dictionary-practice-questions.py
@@ -0,0 +1,7 @@
+'''
+practice questions on dictionary
+1. Create a dictionary of numbers from 1 to 10 and their squares.
+2. Create a dictionary of movies and their release years. Accept user input for the movie name and print the release year.
+3. Create a dictionary of food items and their prices. Write a function to calculate the total price of a list of food items.
+4. Create a food ordering system using a dictionary. The dictionary should contain food items and their prices. Write a function to take an order and calculate the total price.
+'''
